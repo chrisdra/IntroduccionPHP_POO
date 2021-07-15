ccbb7da9cd1cd24f46c66ab275e6c5bbe0518da0
Christian Herrera Cabezas <chrisdra.hc@gmail.com>
2021-07-15 15:04:34 -0400

    Herencia
    Se crea ejemplo se Clases con realizacion a herencia para ocupar el contenido de la clase padre

--- 04.php
+++ 04.php
@@ -1,4 +1,31 @@
 <?php include 'includes/header.php';
 
+class Transporte {
+    public function __construct(protected int $ruedas, protected int $capacidad)
+    {
+        
+    }
+
+    public function getInfo() : string {
+        return "El transporte tiene " . $this->ruedas . " y una capacidad de " . $this->capacidad . " personas ";
+    }
+}
+
+//Esto es herencia
+class Bicicleta extends Transporte {
+
+}
+
+class Automovil extends Transporte {
+
+}
+
+$bicicleta = new Bicicleta(2, 1);
+echo $bicicleta->getInfo();
+
+echo "<hr>";
+
+$auto = new Automovil(4, 4);
+echo $auto->getInfo();
 
 include 'includes/footer.php';
