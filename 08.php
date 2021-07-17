06920b0245eddfc3e255baf610e52ba6e17f62d7
Christian Herrera Cabezas <chrisdra.hc@gmail.com>
2021-07-16 21:54:07 -0400

    Autoload de clases
    Se crea una funcion para poder realizar un llamado automatico a la clase para asi no generar tantas lineas de codigo.

--- 08.php
+++ 08.php
@@ -1,4 +1,19 @@
 <?php include 'includes/header.php';
 
+//Incluir las otras clases
+
+// require 'clases/Cliente.php';
+// require 'clases/Detalles.php';
+
+function mi_autoload($clase) {
+    require __DIR__ . '/clases/' . $clase . '.php';
+}
+
+spl_autoload_register('mi_autoload');
+
+$detalles = new Detalles();
+echo "<hr>";
+$cliente = new Cliente();
+
 
 include 'includes/footer.php';
