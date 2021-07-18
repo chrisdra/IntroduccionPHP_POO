963c7f353c8c0252d0ffff402865cea67be39404
Christian Herrera Cabezas <chrisdra.hc@gmail.com>
2021-07-17 21:35:36 -0400

    Namespaces
    Se modifica el codigo para mostrar el ejemolo de como se trabaja con namespaces

--- 08.php
+++ 08.php
@@ -1,19 +1,20 @@
 <?php include 'includes/header.php';
 
-//Incluir las otras clases
-
 // require 'clases/Cliente.php';
 // require 'clases/Detalles.php';
 
-function mi_autoload($clase) {
-    require __DIR__ . '/clases/' . $clase . '.php';
-}
+use App\Cliente;
+use App\Detalles;
 
+//Incluir las otras clases
+function mi_autoload($clase) {
+    $partes = explode('\\', $clase);
+    require __DIR__ . '/clases/' . $partes[1] . '.php';
+}
 spl_autoload_register('mi_autoload');
 
-$detalles = new Detalles();
-echo "<hr>";
-$cliente = new Cliente();
+$detalles = new App\Detalles();
+$cliente = new App\Cliente();
 
 
 include 'includes/footer.php';
