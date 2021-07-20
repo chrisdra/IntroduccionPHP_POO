16db474db061aff0ec9d7fe86ffd0660cb4216a0
Christian Herrera Cabezas <chrisdra.hc@gmail.com>
2021-07-20 03:06:31 -0400

    Query con PDO
    Se realiza ejemplo de codigo a ejecutar para poder realizar la consulta y coneccion a la base de datos con PDO

--- 10.php
+++ 10.php
@@ -1,4 +1,21 @@
 <?php include 'includes/header.php';
 
+//Conectar a la BD con PDO
+$db = new PDO('mysql:host=localhost; dbname=bienes_raices', 'root', 'root');
+
+$query = "SELECT titulo, imagen FROM propiedades";
+
+$stmt = $db->prepare($query);
+
+$stmt->execute();
+
+$resultado = $stmt->fetchAll( PDO::FETCH_ASSOC );
+
+foreach($resultado as $propiedad):
+    echo $propiedad['titulo'];
+    echo "<br>";
+    echo $propiedad['imagen'];
+    echo "<br>";
+endforeach;
 
 include 'includes/footer.php';
