d484885d916105a90fdc124db76aaff523a16841
Christian Herrera Cabezas <chrisdra.hc@gmail.com>
2021-07-20 02:54:55 -0400

    Consulta BDD
    Se reliza ejemplo de coneccion y de consulta con POO en MySQL

--- 09.php
+++ 09.php
@@ -1,4 +1,23 @@
 <?php include 'includes/header.php';
 
+//Conectar a la BD con MySQLi
+$db = new mysqli('localhost', 'root', 'root', 'bienes_raices');
+
+//Creamos el quey
+$query = "SELECT titulo FROM propiedades";
+
+//Lo preparamos
+$stmt = $db->prepare($query);
+
+//Lo ejecutamos
+$stmt->execute();
+
+//Creamos la variable
+$stmt->bind_result($titulo, $imagen);
+
+//imprimir el resultado
+while($stmt->fetch()):
+    var_dump($titulo);
+endwhile;
 
 include 'includes/footer.php';
